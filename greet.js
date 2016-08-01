1db334ee8b556219e0757068b3e9eabba4bace1d
Kriszti <krisztina.vaci@gmail.com>
2016-08-01 17:29:33 +0200

    b6

--- greet.js
+++ greet.js
@@ -1,11 +1,8 @@
 'use strict';
 
 function greet (firstname, lastname) {
-  console.log(firstname, lastname);
+  console.log('Greetings for the 'firstname, lastname);
 }
 
-<<<<<<< HEAD
+
 greet('Sandor', 'biro');
-=======
-greet();
->>>>>>> e41be3bc830af624c20c8b8e262ab2ae475c7bb2
