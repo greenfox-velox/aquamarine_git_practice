5ac96417a7aeada330bb729dbdd3e3d9d6585594
Kriszti <krisztina.vaci@gmail.com>
2016-08-01 17:00:45 +0200

    jaaj

--- cheer.js
+++ cheer.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 function cheer(name){
   console.log('javscript');
+=======
+function cheer(param){
+  console.log(param);
+>>>>>>> 21aae7f159add34bed1015f934e74927a9683820
 }
 
 greet();
